cd7687f2b715c2a310ab50288726f9bff791207a
Greg Johnston <gcjohnston91@gmail.com>
2020-06-15 18:26:51 -0400

    Auth routes abstracted into seperate file

--- api/server.js
+++ api/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const session = require('express-session');
 const weaponsRouter = require('../routes/weapons');
 const headgearRouter = require('../routes/headgear');
 const userRouter = require('../routes/users');
+const authRouter = require('../auth/authRoutes');
 const helmet = require('helmet'); // security on return headers
 const cors = require('cors'); // cross origin resource sharing
 const server = express();
 const logger = require('../logger');
 server.use(helmet());
 server.use(express.json());
+
+const sessionConfig = {
+  name: 'monster',
+  secret: 'this is not secure',
+  cookie: {
+    maxAge: 1000 * 60 * 60,  // Good for one hour
+    secure: false, // false for dev, true for production
+    httpOnly: true  // no JS access
+  },
+  resave: false,
+  saveUninitialized: true // Set to false in production because of GDPR laws
+};
+
+server.use(session(sessionConfig));
 server.use(cors());
 
 // server.use((req, res, next) => {
@@ -24,6 +40,7 @@ server.get('/api', logger, (req, res) => {
   res.json({message: 'Working'})
 });
 
+server.use('/api/auth', logger, authRouter);
 server.use('/api/weapon', logger, weaponsRouter);
 server.use('/api/headgear', logger, headgearRouter);
 server.use('/api/user', logger, userRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,6 @@
 const express = require('express');
 const Users = require('../models/Users');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-
-router.post('/register', (req, res) => {
-  const credentials = req.body;
-  const {username, password} = credentials;
-  if (!(username && password)){
-    return res.status(400).json({message: 'Username and Password are required'})
-  }
-  credentials.password = bcrypt.hashSync(credentials.password, 12);
-
-  Users.add(credentials)
-    .then(user => {
-      res.status(200).json(user)
-    })
-    .catch(error => {
-      if (error.errno === 19) {
-        res.status(400).json({message: 'Username already taken.'})
-      } else {
-        res.status(500).json(error)
-      }
-    })
-});
-
-router.post('/login', (req, res) => {
-  const {username, password} = req.body;
-  if (!(username && password)){
-    return res.status(400).json({message: 'Username and Password are required'})
-  }
-  Users.findByUserName(username)
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({message: `Welcome back ${username}`})
-      } else {
-        res.status(401).json({message: 'Invalid credentials '})
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error)
-    })
-});
 
 router.get('/', (req, res) => {
   Users.find()
