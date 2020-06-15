6ddc696ccc97be6ec1c599bfd47dca36ffc75488
Greg Johnston <gcjohnston91@gmail.com>
2020-06-15 17:41:59 -0400

    User routes done

--- routes/users.js
+++ routes/users.js
@@ -23,11 +23,22 @@ router.post('/register', (req, res) => {
 
 router.get('/', (req, res) => {
   Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      res.status(500).json({message: 'Unable to get users'})
+    })
+});
+
+router.get('/:username', (req, res) => {
+  const {username} = req.params;
+  Users.findByUserName(username)
     .then(user => {
       res.status(200).json(user)
     })
     .catch(error => {
-      res.status(500).json({message: 'Unable to get users'})
+      res.status(500).json(error)
     })
 });
 
